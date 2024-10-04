1615799a72f0a64d6aba9b6b3022578b404a6ea9
RonEerdekens <roneerdekens@gmail.com>
2024-10-04 21:16:28 +0200

    Pauze
    Pauze
    
    [Ticket: X]

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,9 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('First App'),
-        ),
-        body: const Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Hello World!',
-              ),
-              Text(
-                'It\'s time to learn Flutter!',
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
+  runApp(const MaterialApp(
+    home: Scaffold(
+      body: Center(child: Text('DiddceApp')),
+    ),
+  ));
 }
